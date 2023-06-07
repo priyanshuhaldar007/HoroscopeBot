dae61cbda2bb7f030b83c763e29c4d05c4d12e3f
priyanshuhaldar007 <priyanshu1843@gmail.com>
2023-06-07 17:02:24 +0530

    push code

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+from dotenv import load_dotenv
+import telebot
+import requests
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+# print(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
+
+COMMANDS=[
+    telebot.types.BotCommand('/start','Start the bot'),
+    telebot.types.BotCommand('/horoscope','Know desrised horoscope')
+]
+
+bot.set_my_commands(COMMANDS)
+
+@bot.message_handler(commands=['start','hello'])
+def send_welcome(message):
+    bot.reply_to(message,"Howdy, how are you doing?")
+
+# @bot.message_handler(func=lambda msg:True)
+# def echo_all(message):
+#     bot.reply_to(message,message.text)
+
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(
+        message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign.capitalize())
+
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\n*Sign:* {sign}\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    """Get daily horoscope for a zodiac sign.
+    Keyword arguments:
+    sign:str - Zodiac sign
+    day:str - Date in format (YYYY-MM-DD) OR TODAY OR TOMORROW OR YESTERDAY
+    Return:dict - JSON data
+    """
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
+
+bot.infinity_polling()
